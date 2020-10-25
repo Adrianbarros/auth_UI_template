b38a70a2eb7837834cc89065fd36c6eb914836dd
Adrian Barros <adriansbarros1@gmail.com>
2020-10-25 18:14:43 -0400

    login layout

--- share_party/src/components/AuthForms.js
+++
@@ -1,9 +0,0 @@
-import React, { Component } from 'react'
-
-const AuthForms = () => {
-    return (
-        <h1>Heloooo</h1>
-    );
-}
-
-export default AuthForms;
---
+++ share_party/src/components/authForms/login.jsx
@@ -0,0 +1,38 @@
+import React, { Component } from 'react'
+import PropTypes from 'prop-types'
+
+import loginImg from '../../login.svg'
+
+export class login extends Component {
+    constructor(props) {
+        super(props);
+    }
+
+    render() {
+        return (
+            <div className='base-container'>
+                <div className='header'>login</div>
+                <div className='content'>
+                    <div className="image">
+                        <img src={loginImg} />
+                    </div>
+                    <div className='form'>
+                        <div className='form-group'>
+                            <label htmlFor='username'>Username</label>
+                            <input type='text' name='username' placeholder='Username' />
+                        </div>
+                        <div className='form-group'>
+                            <label htmlFor='password'>Password</label>
+                            <input type='text' name='password' placeholder='Password' />
+                        </div>
+                    </div>
+                </div>
+                <div className='footer'>
+                    <botton type='button' className="btn">Login</botton>
+                </div>
+            </div>
+        )
+    }
+}
+
+
